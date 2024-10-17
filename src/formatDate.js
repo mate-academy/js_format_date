333a69d039c52aa51b275988857a5f9940c59502
Lina Do <dykaya0916@gmail.com>
2024-10-17 21:50:41 +0300

    new solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -10,29 +10,28 @@
 function formatDate(date, fromFormat, toFormat) {
   const separateDate = date.split(fromFormat[3]);
   const newDate = [];
-  const oldYearIndex = fromFormat.indexOf('YY');
   const separator = toFormat[3];
 
   for (let i = 0; i < fromFormat.length - 1; i++) {
     let index = toFormat.indexOf(fromFormat[i]);
 
     if (index === -1) {
-      if (fromFormat[i] === 'YYYY') {
+      if (fromFormat[i].length === 2) {
+        index = toFormat.indexOf('YYYY');
+
+        if (separateDate[i] >= 30) {
+          separateDate[i] = '19' + separateDate[i];
+        }
+
+        if (separateDate[i] < 30) {
+          separateDate[i] = '20' + separateDate[i];
+        }
+      }
+
+      if (fromFormat[i].length === 4) {
         index = toFormat.indexOf('YY');
 
         separateDate[i] = separateDate[i].slice(2);
-      }
-
-      if (fromFormat[i] === 'YY') {
-        index = toFormat.indexOf('YYYY');
-
-        if (separateDate[oldYearIndex] >= 30) {
-          separateDate[oldYearIndex] = '19' + separateDate[oldYearIndex];
-        }
-
-        if (separateDate[oldYearIndex] < 30) {
-          separateDate[oldYearIndex] = '20' + separateDate[oldYearIndex];
-        }
       }
     }
 
