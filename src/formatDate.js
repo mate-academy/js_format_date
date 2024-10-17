c2ef666eff6854601f1ee12f24f7b508d600a31c
Irina <weedfox053@gmail.com>
2024-10-17 17:51:17 +0300

    FormatDate

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,61 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const lastElement = fromFormat[fromFormat.length - 1];
+  const lastElement2 = toFormat[toFormat.length - 1];
+  const dateParts = date.split(lastElement);
+
+  const dateObject = {};
+
+  dateObject[fromFormat[0]] = dateParts[0];
+  dateObject[fromFormat[1]] = dateParts[1];
+  dateObject[fromFormat[2]] = dateParts[2];
+
+  const result = [];
+
+  for (let i = 0; i < toFormat.length; i++) {
+    if (toFormat[i] === 'YY') {
+      if (dateObject['YY']) {
+        result.push(dateObject['YY']);
+      }
+
+      const year = dateObject['YYYY'];
+
+      if (dateObject['YYYY']) {
+        result.push(year.slice(-2));
+      }
+    } else if (toFormat[i] === 'YYYY') {
+      if (dateObject['YY']) {
+        const year2 = dateObject['YY'];
+
+        if (parseInt(year2) < 30) {
+          result.push('20' + year2);
+        } else {
+          result.push('19' + year2);
+        }
+      }
+
+      if (dateObject['YYYY']) {
+        result.push(dateObject['YYYY']);
+      }
+    } else {
+      result.push(dateObject[toFormat[i]]);
+    }
+  }
+
+  const arr = [];
+
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] !== undefined) {
+      arr.push(result[i]);
+    }
+  }
+
+  let arrToString = '';
+
+  arrToString = arr.join(lastElement2);
+
+  return arrToString;
 }
 
 module.exports = formatDate;
