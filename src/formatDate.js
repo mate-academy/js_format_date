6ddfe265c023bd452ea405023e4ca4acc997b0f7
LX-y-ka <alexandravirtualife@gmail.com>
2024-10-26 23:00:33 +0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -9,6 +9,34 @@
  */
 function formatDate(date, fromFormat, toFormat) {
   // write code here
+  const result = Array(3).fill(0);
+  const dateArray = date.split(fromFormat.slice(-1));
+  const day = dateArray[fromFormat.indexOf('DD')];
+  const month = dateArray[fromFormat.indexOf('MM')];
+  const yearFromFormat = fromFormat.indexOf('YY') === -1 ? 'YYYY' : 'YY';
+  const year = dateArray[fromFormat.indexOf(yearFromFormat)];
+  const yearToFormat = toFormat.indexOf('YY') === -1 ? 'YYYY' : 'YY';
+  const formatedYear = format(year, yearFromFormat, yearToFormat);
+
+  result[toFormat.indexOf('DD')] = day;
+  result[toFormat.indexOf('MM')] = month;
+  result[toFormat.indexOf(yearToFormat)] = formatedYear;
+
+  return result.join(toFormat.slice(-1));
+}
+
+function format(year, yearFromFormat, yearToFormat) {
+  if (yearFromFormat === yearToFormat) {
+    return year;
+  }
+
+  if (yearToFormat.length < yearFromFormat.length) {
+    return year.slice(2);
+  }
+
+  if (yearToFormat.length > yearFromFormat.length) {
+    return +year < 30 ? '20' + year : '19' + year;
+  }
 }
 
 module.exports = formatDate;
