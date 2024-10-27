73bc5d3c0801a0da851b2859d2f52d237942e7db
LX-y-ka <alexandravirtualife@gmail.com>
2024-10-27 11:02:25 +0200

    Corrections

--- src/formatDate.js
+++ src/formatDate.js
@@ -10,33 +10,32 @@
 function formatDate(date, fromFormat, toFormat) {
   // write code here
   const result = Array(3).fill(0);
-  const dateArray = date.split(fromFormat.slice(-1));
+  const fromSeparator = date.match(/[^\w\s]/);
+
+  const toSeparator = toFormat[toFormat.length - 1].match(/[^\w\s]/)
+    ? toFormat[toFormat.length - 1]
+    : fromSeparator;
+  const dateArray = date.split(fromSeparator);
   const day = dateArray[fromFormat.indexOf('DD')];
   const month = dateArray[fromFormat.indexOf('MM')];
   const yearFromFormat = fromFormat.indexOf('YY') === -1 ? 'YYYY' : 'YY';
   const year = dateArray[fromFormat.indexOf(yearFromFormat)];
   const yearToFormat = toFormat.indexOf('YY') === -1 ? 'YYYY' : 'YY';
-  const formatedYear = format(year, yearFromFormat, yearToFormat);
+  let formatedYear = year;
+
+  if (yearToFormat.length < yearFromFormat.length) {
+    formatedYear = year.slice(2);
+  }
+
+  if (yearToFormat.length > yearFromFormat.length) {
+    formatedYear = +year < 30 ? '20' + year : '19' + year;
+  }
 
   result[toFormat.indexOf('DD')] = day;
   result[toFormat.indexOf('MM')] = month;
   result[toFormat.indexOf(yearToFormat)] = formatedYear;
 
-  return result.join(toFormat.slice(-1));
-}
-
-function format(year, yearFromFormat, yearToFormat) {
-  if (yearFromFormat === yearToFormat) {
-    return year;
-  }
-
-  if (yearToFormat.length < yearFromFormat.length) {
-    return year.slice(2);
-  }
-
-  if (yearToFormat.length > yearFromFormat.length) {
-    return +year < 30 ? '20' + year : '19' + year;
-  }
+  return result.join(toSeparator);
 }
 
 module.exports = formatDate;
