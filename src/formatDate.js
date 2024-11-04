e7bc4efa23b3845059b34042a5cfdb21096b6fbe
Yevhenii Kucherenko <102630213+chelsea7smile@users.noreply.github.com>
2024-11-04 17:02:41 +0200

    Update formatDate.js
    define constants for year formats

--- src/formatDate.js
+++ src/formatDate.js
@@ -13,16 +13,21 @@ function formatDate(date, fromFormat, toFormat) {
   const receivedDate = {}; // create an empty object
   const changedDate = [];
 
+  // define constance for year formats
+  const YEAR_2_DIGITS = 'YY';
+  const YEAR_4_DIGITS = 'YYYY';
+  
+
   // check each part of date(YY or YYYY) and saving year value to receivedDate;
   for (let i = 0; i < fromFormat.length - 1; i++) {
     switch (fromFormat[i]) {
-      case 'YYYY':
-        receivedDate['YY'] = dateArr[i].slice(2);
-        receivedDate['YYYY'] = dateArr[i];
+      case YEAR_4_DIGITS:
+        receivedDate[YEAR_2_DIGITS] = dateArr[i].slice(2);
+        receivedDate[YEAR_4_DIGITS] = dateArr[i];
         break;
-      case 'YY':
-        receivedDate['YYYY'] = `${dateArr[i] < 30 ? 20 : 19}${dateArr[i]}`;
-        receivedDate['YY'] = dateArr[i];
+      case YEAR_2_DIGITS:
+        receivedDate[YEAR_4_DIGITS] = `${dateArr[i] < 30 ? 20 : 19}${dateArr[i]}`;
+        receivedDate[YEAR_2_DIGITS] = dateArr[i];
         break;
       default:
         receivedDate[fromFormat[i]] = dateArr[i];
