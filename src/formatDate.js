7bc4e2d00b307cd4ee9df0763655b45eab361f42
TargoniyAnatoliy <targoniyanatoliy@gmail.com>
2024-11-05 15:09:19 +0200

    add solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,8 +7,52 @@
  *
  * @returns {string}
  */
+
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const inputSeparator = fromFormat[fromFormat.length - 1];
+  const inputDate = date.split(inputSeparator);
+  const outputSeparator = toFormat[toFormat.length - 1];
+  const inputYear = fromFormat.includes('YY')
+    ? inputDate[fromFormat.indexOf('YY')]
+    : inputDate[fromFormat.indexOf('YYYY')];
+  const inputMonth = inputDate[fromFormat.indexOf('MM')];
+  const inputDay = inputDate[fromFormat.indexOf('DD')];
+
+  const outputDate = [];
+
+  outputDate.length = toFormat.length - 1;
+
+  for (let i = 0; i < outputDate.length; i++) {
+    // перебираємо шаблон вихідної дати
+    if (toFormat[i] === 'YYYY') {
+      if (inputYear.length === 4) {
+        outputDate[i] = inputYear;
+        continue;
+      }
+
+      if (inputYear.length === 2) {
+        outputDate[i] = inputYear < 30 ? `20${inputYear}` : `19${inputYear}`;
+        continue;
+      }
+    }
+
+    if (toFormat[i] === 'YY') {
+      outputDate[i] = inputYear.slice(2);
+      continue;
+    }
+
+    if (toFormat[i] === 'MM') {
+      outputDate[i] = inputMonth;
+      continue;
+    }
+
+    if (toFormat[i] === 'DD') {
+      outputDate[i] = inputDay;
+      continue;
+    }
+  }
+
+  return outputDate.join(outputSeparator);
 }
 
 module.exports = formatDate;
