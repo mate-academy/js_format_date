23860cb560aa87f7bedb834a54c81e52a2fed5a9
Oksana Vasylyshyn <oksanamow@gmail.com>
2024-11-13 19:30:37 +0200

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,34 +8,57 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const fromSeparator = fromFormat[3];
-  const toSeparator = toFormat[3];
+  const splitElement = fromFormat[3];
+  const joinElement = toFormat[3];
+  const newDate = date.split(splitElement);
 
-  const dateParts = date.split(fromSeparator);
+  const formatAgan = [];
 
-  const dateObject = {};
-
-  fromFormat.forEach((part, index) => {
-    if (part !== fromSeparator) {
-      dateObject[part] = dateParts[index];
+  for (let i = 0; i < fromFormat.length - 1; i++) {
+    if (fromFormat[i] === 'YY' || fromFormat[i] === 'YYYY') {
+      formatAgan[0] = newDate[i];
     }
-  });
 
-  if (fromFormat.includes('YYYY') && toFormat.includes('YY')) {
-    dateObject['YY'] = dateObject['YYYY'].slice(-2);
-  } else if (fromFormat.includes('YY') && toFormat.includes('YYYY')) {
-    const year = parseInt(dateObject['YY'], 10);
+    if (fromFormat[i] === 'MM') {
+      formatAgan[1] = newDate[i];
+    }
 
-    dateObject['YYYY'] =
-      year < 30 ? `20${dateObject['YY']}` : `19${dateObject['YY']}`;
+    if (fromFormat[i] === 'DD') {
+      formatAgan[2] = newDate[i];
+    }
   }
 
-  const formattedDate = toFormat
-    .filter((part) => part !== toSeparator)
-    .map((part) => dateObject[part])
-    .join(toSeparator);
+  let year = formatAgan[0];
+  const month = formatAgan[1];
+  const day = formatAgan[2];
 
-  return formattedDate;
+  if (toFormat.includes('YYYY') && year.length === 2 && year < 30) {
+    year = `${20}` + `${year}`;
+  }
+
+  if (toFormat.includes('YYYY') && year.length === 2 && year >= 30) {
+    year = `${19}` + `${year}`;
+  }
+
+  if (toFormat.includes('YY')) {
+    year = year.toString().slice(-2);
+  }
+
+  for (let i = 0; i < toFormat.length - 1; i++) {
+    if (toFormat[i] === 'YY' || toFormat[i] === 'YYYY') {
+      formatAgan[i] = year;
+    }
+
+    if (toFormat[i] === 'MM') {
+      formatAgan[i] = month;
+    }
+
+    if (toFormat[i] === 'DD') {
+      formatAgan[i] = day;
+    }
+  }
+
+  return formatAgan.join(joinElement);
 }
 
 module.exports = formatDate;
