9ed8fbbb8c311dd0ed842f7ccb65cfd5e39ce2e7
Solomiia Palka <solyaqw@gmail.com>
2024-11-18 10:26:49 +0200

    add copys

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,27 +8,32 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const separator = fromFormat.pop();
+  const fromFormatCopy = [...fromFormat];
+  const toFormatCopy = [...toFormat];
+
+  const separator = fromFormatCopy.pop();
   const parts = date.split(separator);
 
   const dateParts = {};
 
-  fromFormat.forEach((part, index) => {
+  fromFormatCopy.forEach((part, index) => {
     dateParts[part] = parts[index];
   });
 
-  if (toFormat.includes('YY') && dateParts['YYYY']) {
+  if (toFormatCopy.includes('YY') && dateParts['YYYY']) {
     dateParts['YY'] = dateParts['YYYY'].slice(-2);
   }
 
-  if (toFormat.includes('YYYY') && dateParts['YY']) {
+  if (toFormatCopy.includes('YYYY') && dateParts['YY']) {
     dateParts['YYYY'] =
       dateParts['YY'] < '30' ? '20' + dateParts['YY'] : '19' + dateParts['YY'];
   }
 
-  const newSeparator = toFormat.pop();
+  const newSeparator = toFormatCopy.pop();
 
-  const newDate = toFormat.map((part) => dateParts[part]).join(newSeparator);
+  const newDate = toFormatCopy
+    .map((part) => dateParts[part])
+    .join(newSeparator);
 
   return newDate;
 }
