ac4b85268ffc991ae6110d107cff153242d8588c
Maksym Tereshyn <max.tereshyn@gmail.com>
2024-11-21 12:37:11 +0100

    add task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,33 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromFormatCopy = [...fromFormat];
+  const toFormatCopy = [...toFormat];
+  const deleteLastElement = fromFormatCopy.pop();
+  const partOfDate = date.split(deleteLastElement);
+  const partsOfDate = {};
+
+  fromFormatCopy.forEach((part, index) => {
+    partsOfDate[part] = partOfDate[index];
+  });
+
+  if (toFormatCopy.includes('YY') && partsOfDate['YYYY']) {
+    partsOfDate['YY'] = partsOfDate['YYYY'].slice(-2);
+  }
+
+  if (toFormatCopy.includes('YYYY') && partsOfDate['YY']) {
+    partsOfDate['YYYY'] =
+      partsOfDate['YY'] < '30'
+        ? '20' + partsOfDate['YY']
+        : '19' + partsOfDate['YY'];
+  }
+
+  const newDeleteLastElement = toFormatCopy.pop();
+  const newDate = toFormatCopy
+    .map((part) => partsOfDate[part])
+    .join(newDeleteLastElement);
+
+  return newDate;
 }
 
 module.exports = formatDate;
