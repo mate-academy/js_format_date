f19e6015175e4998a5c4719dd74df2f1a1527c78
Volodymyr Makhonin <zavozi@gmail.com>
2024-12-11 12:47:59 +0200

    fix mentor`s review

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,7 +7,7 @@
  *
  * @returns {string}
  */
-function formatDate(date, fromFormat, toFormat) {
+function formatDate(date, fromFormat, toFormat, cutoffYear = 30) {
   const fromSeparator = fromFormat[3];
   const toSeparator = toFormat[3];
   const dateParts = date.split(fromSeparator);
@@ -25,7 +25,7 @@ function formatDate(date, fromFormat, toFormat) {
     } else if (key === 'YY') {
       newFormat.push(dateSet.YYYY.slice(2));
     } else if (key === 'YYYY') {
-      if (dateSet.YY < 30) {
+      if (dateSet.YY < cutoffYear) {
         newFormat.push('20' + dateSet.YY);
       } else {
         newFormat.push('19' + dateSet.YY);
