c7d7a771b3425f38abbf2f31dafb72b880dcefba
gabai62 <gabai62it@gmail.com>
2024-12-19 15:50:23 +0200

    task done

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,43 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const [toParts] = [toFormat.slice()];
+  const [fromParts] = [fromFormat.slice()];
+  const separators = ['-', '/', ' ', '.'];
+  const separator = findSeparator(date, separators);
+  const newSeparator = findSeparator(toFormat, separators);
+
+  function findSeparator(enterDate, separatorsArr) {
+    for (let i = 0; i < separatorsArr.length; i++) {
+      if (enterDate.includes(separatorsArr[i])) {
+        return separatorsArr[i];
+      }
+    }
+  }
+
+  const dateParts = date.split(separator);
+  const dateFrom = {};
+  const dateTo = [];
+
+  for (let i = 0; i < fromFormat.length; i++) {
+    dateFrom[fromParts[i]] = dateParts[i];
+  }
+
+  if (dateFrom.YYYY) {
+    dateFrom.YY = dateFrom.YYYY.slice(-2);
+  } else if (dateFrom.YY) {
+    if (dateFrom.YY < 30) {
+      dateFrom.YYYY = `20${dateFrom.YY}`;
+    } else {
+      dateFrom.YYYY = `19${dateFrom.YY}`;
+    }
+  }
+
+  for (let i = 0; i < toParts.length - 1; i++) {
+    dateTo.push(dateFrom[toParts[i]]);
+  }
+
+  return dateTo.join(newSeparator);
 }
 
 module.exports = formatDate;
